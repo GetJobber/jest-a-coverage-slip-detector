835f4d038dc45535f65521cc673bfd3928bd069f
Sean Healy <s@xib.ca>
2023-12-01 12:35:26 -0700

    fix: typing on withJestSlipDetection

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,13 @@
+import type { Config } from "@jest/types";
+
 declare module "@jobber/jest-a-coverage-slip-detector" {
   export function runPerFileCoverageTest(): boolean;
 
   export function updatePerFileCoverageExceptions(forceUpdate: boolean): void;
 
-  export function withJestSlipDetection(jestConfig: {
+  export function withJestSlipDetection(jestConfig: Config.InitialOptions & {
     collectCoverageFrom?: string[];
-  }): any;
+  }): Config.InitialOptions;
 
   export function mergeCoverageAndGenerateReports(): void;
 }
